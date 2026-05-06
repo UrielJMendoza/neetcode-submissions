3a04e9898df547ba7ed7f90be8606f27cb344efb
Uriel Mendoza <mend9986@bears.unco.edu>
2026-05-05 21:58:38 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 11
+print(type(variable))
+variable = 4.4
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "fhf"
+print(type(variable))
+variable = [2,2]
+print(type(variable))
