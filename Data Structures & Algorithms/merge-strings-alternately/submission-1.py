a8656aacab88a9fdb6c8b2ae767dfd992d684199
Uriel Jeramiah Mendoza <jeramiahblinks@gmail.com>
2026-05-25 17:30:42 -0600

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+
+
+        new = ""
+
+        lens1 = len(word1)
+        lens2 = len(word2)
+
+
+        short = 0
+        biggest = 0
+
+
+        if lens1 >= lens2:
+            short = lens2
+            biggest = word1
+        else:
+            short = lens1
+            biggest = word2
+
+        
+
+        for i in range(short):
+            new += word1[i]
+            new += word2[i]
+
+        if lens1 >= lens2:
+            for i in range(short, lens1):
+                new += word1[i]
+        else:
+            for i in range(short, lens2):
+                new += word2[i]
+
+
+
+        return new
+
+
+        
